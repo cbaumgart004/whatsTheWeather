84f3fba0a49f495ef94b9c6db5e48efa665da347
[Chris B] <[baumgart.c@gmail.com]>
2024-06-20 10:23:27 -0600

    Both Fetch requests functioning now

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,11 @@ console.log(`link looking good`);
 const cityName = document.getElementById('cityName');
 const searchButton = document.getElementById('searchButton');
 //define variable for APIKey
-const APIKey = `04226ea09cd7c111ba2c55a955a667b4`;
+const APIKey = `b657affac026785e279a481f8fd5894c`;
 
 //fetch request
 const getWeather = function() {
-    console.log(`got weather?`)
+    console.log(`got weather?`);
     let city = 'Denver';
     const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
@@ -19,13 +19,37 @@ const getWeather = function() {
         }
     }).then(function (data) {
         console.log(data);
+        console.log(data.coord);
+        fiveDayForecast(data.coord);
     })
     .catch (function(error){
         console.log(error);
         alert('Unable to reach Weather API');
     });
 
+};
 
+const fiveDayForecast = function(coordinate) {
+    const coordinatesArray = [];
+    coordinatesArray.push(coordinate);
+    const lat = coordinatesArray[0].lat;
+    const lon = coordinatesArray[0].lon;
+
+    const forecastUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${APIKey}`;
+
+    fetch(forecastUrl).then(function(response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert(`Error: ${response.statusText}`);
+            };
+    }).then(function(data) {
+        console.log(data);
+    })
+    .catch (function (error) {
+        console.log(error);
+        alert('Unable to reach Weather API');
+    });
 };
 //event listener for Search Button
 searchButton.addEventListener('click', function(event) {
