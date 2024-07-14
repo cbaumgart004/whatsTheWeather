51e2c8cba836ca6d0b41336dc3ab126570c217d8
[Chris B] <[baumgart.c@gmail.com]>
2024-07-14 14:18:09 -0600

    rendered current search and search history as clickable buttons

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 
 //define variables for html elements
+let currentCity = '';
+let currentState = '';
 const cityName = document.getElementById('cityName');
 const searchButton = document.getElementById('searchButton');
 const futureForecast = document.getElementById('resultsFiveDay');
@@ -8,11 +10,11 @@ const state = document.getElementById('state');
 const searchHistory = document.getElementById('searchHistory');
 //define variable for APIKey
 const APIKey = `b657affac026785e279a481f8fd5894c`;
-const history = JSON.parse(localStorage.getItem('history'))||[]
+const history = JSON.parse(localStorage.getItem('history'))||[];
 
 //fetch request
 const getWeather = function(cityName, stateName) {
-    console.log(`got weather?`);
+    
     let city = cityName;
     let state = stateName;
     const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},3166-2&appid=${APIKey}`;
@@ -61,11 +63,9 @@ const fiveDayForecast = function(lattitude, longitude) {
 
 const parseWeather = function (weather) {
     console.log(weather.current);
-    let time = dayjs.unix(weather.current.dt).format('MMM D, YYYY, hh:mm:ss a');
-    let temp = parseInt(weather.current.temp);
+    
     let weatherDaily = (weather.current.weather[0].description);
-    console.log(time);
-    console.log(temp);
+    
     console.log(weatherDaily);
     renderCurrentForecast(weather);
 };
@@ -81,45 +81,117 @@ const parseWeeklyForecast = function (weather) {
     renderWeeklyForecast(thisWeek);
 };
 const renderCurrentForecast = function (weather) {
-    const forecastToday = document.createElement('div');
-    forecastToday.textContent = weather.current.weather[0].description
+    let time = dayjs.unix(weather.current.dt).format('dddd, MMM D, YYYY');
+    let temp = parseInt(weather.current.temp);
+    let weatherNow = weather.current.weather[0];
+    console.log(`your weather now variable${weatherNow}`);
+    const forecastTodayCard = document.createElement('div');
+    forecastTodayCard.classList.add('largeCard');
+    currentCity = localStorage.getItem('currentCity');
+    currentState = localStorage.getItem('currentState');
+    const header = document.createElement('h2');
+    const weatherDescription = document.createElement('p');
+    const todayTemp = document.createElement('p');
+    const displayTime = document.createElement('p');
+    const displayWind = document.createElement('p');
+    const displayHumid = document.createElement('p');
+    forecastTodayCard.style.backgroundImage = `url(https://openweathermap.org/img/wn/${weatherNow.icon}@2x.png)`
+    //Set content to created elements
+    header.textContent = `${currentCity}, ${currentState}`;
+    weatherDescription.textContent = `Today you can expect ${weatherNow.description}`;
+    todayTemp.textContent = `${temp}\u00B0 F`
+    displayTime.textContent = time;
+    displayWind.textContent = displayWind.textContent = `Wind Speed:${parseInt(weather.current.wind_speed)} mph`;
+    displayHumid.textContent = `Humidity: ${weather.current.humidity}%`;
+    console.log(`Today's forecast for ${header.textContent}`);
+    
+
+    
+    forecastTodayCard.appendChild(header);
+    forecastTodayCard.appendChild(displayTime);
+    forecastTodayCard.appendChild(weatherDescription);
+    forecastTodayCard.appendChild(todayTemp);
+    forecastTodayCard.appendChild(displayWind);
+    forecastTodayCard.appendChild(displayHumid);
+
+    forecastToday.appendChild(forecastTodayCard);
 }
 
 const renderWeeklyForecast = function (forecastArray) {
     for (let i = 0; i < forecastArray.length; i++) {
         const forecastDay = document.createElement('div');
-
-        console.log(dayjs.unix(forecastArray[i].dt).format('MMM D, YYYY'))
-        console.log(forecastArray[i].summary)
-        forecastDay.textContent = `${parseInt(forecastArray[i].temp.day)} \u00B0F ${forecastArray[i].summary}`;
+        //create elements of the div
+        const displayWeekday = document.createElement('h3');
+        const displayTemp = document.createElement('p1');
+        const displayDesc = document.createElement('p');
+        const displayWind = document.createElement('p');
+        const displayHumid = document.createElement('p');
+        //update text of items
+        let weekday = dayjs.unix(forecastArray[i].dt).format('ddd')
+        displayWeekday.textContent = weekday;
+        displayTemp.textContent = `${parseInt(forecastArray[i].temp.day)}\u00B0 F`;
+        displayDesc.textContent = `${forecastArray[i].summary}`;
+        console.log(`Wind today ${forecastArray[i].wind_speed}`);
+        displayWind.textContent = `Wind Speed: ${parseInt(forecastArray[i].wind_speed)} mph`;
+        displayHumid.textContent = `Humidity: ${forecastArray[i].humidity}%`;
         let dailyIcon = forecastArray[i].weather[0].icon;
-        console.log(dailyIcon);
-
+        
+        //set icon from api and add a class
         forecastDay.style.backgroundImage = `url(https://openweathermap.org/img/wn/${dailyIcon}@2x.png)`;
         forecastDay.classList.add('card');
+        //add elements to div
+        forecastDay.appendChild(displayWeekday);
+        forecastDay.appendChild(displayTemp);
+        forecastDay.appendChild(displayDesc);
+        forecastDay.appendChild(displayWind);
+        forecastDay.appendChild(displayHumid);
+        //update dom with new div
         futureForecast.appendChild(forecastDay);
         
     }
 };
 
 const renderHistory = function (city, state) {
-    history.push({'city': city, 'state': state});
+    
+    
     localStorage.setItem('history', JSON.stringify(history));
     //iterate through history array and create/append elements
     for (let i=0; i<history.length;i++) {
-        const historyElement = document.createElement('div');
+        const historyElement = document.createElement('button');
+        historyElement.classList.add('btn');
         historyElement.textContent = `${history[i].city}, ${history[i].state}`
+        historyElement.setAttribute('city', history[i].city);
+        historyElement.setAttribute('state', history[i].state);
         searchHistory.appendChild(historyElement)
     };
-    console.log (`history: ${JSON.stringify(history)}`);
+    
 };
+//Function to clear previous search
+const clear = function() {
+    forecastToday.innerHTML = '';
+    futureForecast.innerHTML = '';
+    searchHistory.innerHTML = '';
+}
 //event listener for Search Button
 searchButton.addEventListener('click', function(event) {
     event.preventDefault();
     if (!cityName.value || !state.value) {
         alert(`Please enter both city and state`)
     } else {
+        currentCity = cityName.value
+        currentState = state.value
+        //set local storage for the current city to be used in other functions
+        localStorage.setItem('currentCity', currentCity);
+        localStorage.setItem('currentState', currentState);
+        
         getWeather(cityName.value, state.value);
+        //Only Return last 15 searches
+        if (history.length>14){
+            history.shift();
+        }
+        history.push({'city': cityName.value, 'state': state.value});
+        //clear elements in the page before performing a new search
+        clear();
         renderHistory(cityName.value, state.value);
         console.log(cityName.value);
         console.log(state.value);
@@ -128,3 +200,21 @@ searchButton.addEventListener('click', function(event) {
     };
     
 });
+
+searchHistory.addEventListener('click', function(event){
+    if(event.target.tagName === 'BUTTON') {
+        let historicCity = event.target.getAttribute('city');
+        let historicState = event.target.getAttribute('state');
+        localStorage.setItem('currentCity', historicCity);
+        localStorage.setItem('currentState', historicState);
+        console.log(`from you history: ${historicCity}`)
+        clear();
+        getWeather(historicCity, historicState);
+        renderHistory();
+        
+    }
+});
+
+renderHistory();
+
+//localStorage.clear();
